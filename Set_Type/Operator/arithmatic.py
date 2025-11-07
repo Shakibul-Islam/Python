6340330cf20b4a35ae96943c5b22c337afc5bc10
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-07 12:32:20 +0600

    Arithmatic Operator

---
+++ Set_Type/Operator/arithmatic.py
@@ -0,0 +1,24 @@
+x= 20
+y = 10
+ 
+print("Addition:", x + y) # Addition
+
+print("Subtraction:", x - y) # Subtraction
+
+print("Multiplication:", x * y) # Multiplication
+
+print("Division:", x / y) # Division
+
+print("Floor Division:", x // y) # Floor Division
+
+print("Modulus:", x % y) # Modulus
+
+print("Exponentiation:", x ** y) # Exponentiation
+
+print("Negation of x:", -x) # Negation
+
+print("Positive of y:", +y) # Positive
+
+# Combined Assignment
+x += 5
+print("After x += 5:", x)
