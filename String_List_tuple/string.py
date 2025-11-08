2976fba7e21e2b3f5674395a51cd7c270a1e0e13
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-08 19:43:46 +0600

    String

---
+++ String_List_tuple/string.py
@@ -0,0 +1,13 @@
+name_1 = 'Shakibul'
+
+name_2 = "Rakibul"
+
+name_3 = '''
+This is a multi-line
+string example.
+'''
+
+name_4 = """Another example of
+a multi-line string."""
+
+print(name_1, name_2, name_3, name_4)
