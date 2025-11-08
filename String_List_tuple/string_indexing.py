89112ec4707357cbd8e6dac19d2159a73cfde914
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-08 19:53:04 +0600

    Add comment

--- String_List_tuple/string_indexing.py
+++ String_List_tuple/string_indexing.py
@@ -1,8 +1,8 @@
 name = "shakibul islam"
 
-print(name[0])      # s
+print(name[0])      # s left to right indexing
 print(name[3])      # k
-print(name[-1])     # m
+print(name[-1])     # m right to left indexing
 print(name[-4])     # l
 
 print(len(name))  # 14
