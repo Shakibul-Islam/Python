66fef9c17ceec9eac155222ae900307b8b6609e5
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-09 04:33:34 +0600

    slicing and membership

---
+++ String_List_tuple/slicing.py
@@ -0,0 +1,10 @@
+"""
+string[start:end:step]
+
+start = 0 # default
+end = len(string) # default  (exclusive)
+step = 1 # default
+"""
+text = "Hello, welcome to the world of Python programming."
+# Slicing examples
+print(text[1:15:1])   # ello, welcome
