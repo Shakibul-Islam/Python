2d3adf7b126922ac25101d207cc24496a371963c
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-09 13:48:17 +0600

    more code for split

--- Searchng & Replacing Methods/split.py
+++ Searchng & Replacing Methods/split.py
@@ -1,2 +1,6 @@
 str = "a,b,c,d,e"
 print(str.split(","))  # Output: ['a', 'b', 'c', 'd', 'e'] #output as list
+print(str.split(",", 2))  # Output: ['a', 'b', 'c,d,e'] #split at most 2 times
+
+print(str.rsplit(",", 2))  # Output: ['a,b,c', 'd', 'e'] #split at most 2 times from right
+print(str.splitlines())  # Output: ['a,b,c,d,e'] #no line breaks, so entire string as single element
