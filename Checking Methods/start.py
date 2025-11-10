fe8465504fbc752803c53fa5d687bd09b4b33fa2
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-10 11:28:33 +0600

    isdigit, isalnum

--- Checking Methods/start.py
+++ Checking Methods/start.py
@@ -1,9 +1,16 @@
-str = "Python is great"
-print(str.startswith("P")) # Output: True #checks if string starts with 'P'
+# str = "Python is great"
+# print(str.startswith("P")) # Output: True #checks if string starts with 'P'
 
-print(str.endswith("t")) # Output: True #checks if string ends with 't'
-print(str.startswith("is", 7)) # Output: True #checks if substring starting at index 7 starts with 'is'
-print(str.endswith("is", 0, 7)) # Output: True #checks if substring from index 0 to 7 ends with 'is'
+# print(str.endswith("t")) # Output: True #checks if string ends with 't'
+# print(str.startswith("is", 7)) # Output: True #checks if substring starting at index 7 starts with 'is'
+# print(str.endswith("is", 0, 7)) # Output: True #checks if substring from index 0 to 7 ends with 'is'
 
-print(str.isalpha()) # Output: False #checks if all characters are alphabetic
-print("Python".isalpha()) # Output: True #checks if all characters are alphabetic
+# print(str.isalpha()) # Output: False #checks if all characters are alphabetic
+# print("Python".isalpha()) # Output: True #checks if all characters are alphabetic
+
+str1 = "12345"
+print(str1.isdigit())  # Output: True #checks if all characters are digits
+print("123a45".isdigit())  # Output: False #checks if all characters are digits
+
+str2 = "abs123"
+print(str2.isalnum())
