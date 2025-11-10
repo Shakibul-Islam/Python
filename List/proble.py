535bbdcac78187a8317bc8baaebb46aa928e5639
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-10 12:30:38 +0600

    List initialization

---
+++ List/proble.py
@@ -0,0 +1,20 @@
+name = "Shakibul"
+age = 25
+
+blood_group = "O+"
+marks = 88.5
+
+#list = one variable  but contain multiple values with  different data tape
+
+# Ordered
+# mutable
+# dynamic  size (list size increase or decrease)
+# heterogeneous data type # store different data type value
+
+
+my_list = [name, age, blood_group, marks]
+print(my_list)
+
+my_list1 = [1 ,2, 3, 3, 4, "Shakibul", 5.6, True]
+print(my_list1)
+print(type(my_list1))
