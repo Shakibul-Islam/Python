a2f783762bc0fdd0af0e66e24401bb2ecc15fdb7
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-11 20:40:38 +0600

    Create List by range and List compression by for loop help of range()

---
+++ List/create_list_by_range.py
@@ -0,0 +1,8 @@
+number = list(range(1, 21, 2))  # start, stop, step
+
+print("List of odd numbers from 1 to 20:", number)
+# Output: List of odd numbers from 1 to 20: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+even_number = list(range(2, 21, 2))  # start, stop, step
+print("List of even numbers from 2 to 20:", even_number)
+# Output: List of even numbers from 2 to 20: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
---
+++ List/list_compression.py
@@ -0,0 +1,12 @@
+squre_list = []
+for i in range(1,12):
+    squre_list.append(i**2)
+print("List of squares from 1 to 11:", squre_list)
+
+# List comprehension to create the same list of squares
+squre_comp = [i**2 for i in range(1, 12)]
+print("List of squares using list comprehension:", squre_comp)
+
+print("1 t0 21 odd number:", [i for i in range(1, 22, 2)])  # odd numbers from 1 to 21
+
+print("2 to 22 even number:", [j for j in range(2, 22, 2) ]) # even numbers from 2 to 22
