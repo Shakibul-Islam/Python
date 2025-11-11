38e5060ffdbedb7d2c7f1b73a900f0aca7337ca1
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-11 19:49:03 +0600

    nasted List ans access nasted list

---
+++ List/nasted_list.py
@@ -0,0 +1,8 @@
+a = [1, 2, 3, 4, 5]
+b = [4, 5, 6, 7, 8, ["Shakibul", 12 ,13], [22, "Java", 24]]
+# Accessing elements in nested lists
+print("First element of a:", a[0])
+print("Second element of b:", b[1])
+print("First element of nested list in b:", b[5][0]) # Accessing "shakibul"
+print("Second element of nested list in b:", b[6][1]) # Accessing "Java"
+
