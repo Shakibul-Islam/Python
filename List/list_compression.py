0ae93effb88981abc788d0039a8689e714f6294f
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-14 12:35:50 +0600

    tuple txt

--- List/list_compression.py
+++ List/list_compression.py
@@ -4,9 +4,10 @@ for i in range(1,12):
 print("List of squares from 1 to 11:", squre_list)
 
 # List comprehension to create the same list of squares
-squre_comp = [i**2 for i in range(1, 12)]
+squre_comp = [i**2 for i in range(1, 12) if i%2 == 0] 
 print("List of squares using list comprehension:", squre_comp)
 
 print("1 t0 21 odd number:", [i for i in range(1, 22, 2)])  # odd numbers from 1 to 21
 
 print("2 to 22 even number:", [j for j in range(2, 22, 2) ]) # even numbers from 2 to 22
+
