92997556757ffbbf6e68c45b559a2a9add1048d2
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-16 20:27:50 +0600

    Create a Dictionary

---
+++ Dictionary/create_dictionary.py
@@ -0,0 +1,13 @@
+# [] Use to Create a List
+# () Use to Create a Tuple
+# {} Use to Create a Dictionary
+
+my_dectionary = {
+    "Name": "Shakibul Islam",
+    "Age": 25,
+    "Mark": 50.5,
+    "Marks": [50, 60, 70, 80],
+    "Country": "Bangladesh"
+}
+
+print(my_dectionary)
