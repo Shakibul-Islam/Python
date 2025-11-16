c394c11279cf4c09081c09cf63655c3bc8aad620
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-16 23:35:18 +0600

    keys and values function

---
+++ Dictionary/Methods/keys.py
@@ -0,0 +1,10 @@
+profile = {
+    'name': 'Alice',
+    'age': 30,  
+    'salary': 70000
+}
+keys = profile.keys()
+print(keys)  # Output: dict_keys(['name', 'age', 'salary'] )
+
+value = profile.values()
+print(list(value))  # Output: dict_values(['Alice', 30, 70000] )
