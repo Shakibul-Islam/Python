08b9ea9404389c0c68467489f49909086650d8b9
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-16 21:37:47 +0600

    Delete a list from Dictionary

---
+++ Dictionary/delete.py
@@ -0,0 +1,9 @@
+my_dict = {
+    'name': 'python',
+    'version': 3.10,
+    'Use_Case': ['AI', 'ML' 'DS', 'Web Development']
+}
+
+print(my_dict)
+del my_dict['version']  # Deleting a key-value pair using del keyword
+print(my_dict)
