3fc37c7204bb13b9f8f4e1fc06b12787354529bb
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-17 21:29:51 +0600

    Parameter and Argument

---
+++ Functions/parameter&Arguments.py
@@ -0,0 +1,12 @@
+def greet(name):    # function with parameter and name - parameter
+    print("Hi", name + "! Welcome to the Python functions module!")
+
+greet("Shakibul")  # Calling the function to display the greeting message
+greet("Rakibul") # Rakibul - argument
+greet("Farzana")
+
+# Argument are 3 types
+# 1. Positional Argument
+# 2. Keyword Argument
+# 3. Default Argument
+
