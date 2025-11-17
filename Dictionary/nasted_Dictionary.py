0981b12b34dbacc78222fb4c921027f19ff3e3c0
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-17 12:51:20 +0600

    Nasted Dictionary

---
+++ Dictionary/nasted_Dictionary.py
@@ -0,0 +1,14 @@
+programming_languages = {
+    "Python": {
+        'name': "Python", 
+        'year_created': 1991, 
+        'Use_Case': ['Web Development', 'Data Science', 'Scripting']
+        },
+    "Java": {
+        'name': "Java",
+        'year_created': 1995, 
+        'Use_Case': ['Enterprise Applications', 'Mobile Applications', 'Web Development']
+        }
+}
+
+print(programming_languages)
