35d5c9f209e970ba09c7977ff8f5844032f53bf6
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-17 23:02:54 +0600

    return keyword

---
+++ Functions/return.py
@@ -0,0 +1,8 @@
+def get_full_name(first_name, last_name):
+    full_name = f'{first_name} {last_name}'
+    #print(full_name)
+    return full_name
+    
+
+name = get_full_name("Shakibul", "Islam")
+print(name)
