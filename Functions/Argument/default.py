e748aa58aa8a34ea22cf5a76796f15ca94ee5a8d
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-17 22:55:11 +0600

    add one another function call

--- Functions/Argument/default.py
+++ Functions/Argument/default.py
@@ -2,3 +2,5 @@ def greet(name ="Shakibul", city="Rangamati"):  # default arguments
     print(f'Welcome {name} to the {city}')
 
 greet() # Default arguments
+
+greet(name ="Shahriar", city = "Rangpur")
