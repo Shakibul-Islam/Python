9eb47dd6207d60e757cb1f5584270ba0549381a9
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-11-17 06:38:59 +0600

    delete all Dictionary by clear() function

---
+++ Dictionary/Methods/clear.py
@@ -0,0 +1,11 @@
+profile = {
+    'name': 'Alice',
+    'age': 30,  
+    'salary': 70000
+}
+
+print(f"Original Profile: {profile}")
+
+cleares = profile.clear()
+print(f"Cleared Dictionary: {cleares}")
+print(f"Profile after clear(): {profile}")
