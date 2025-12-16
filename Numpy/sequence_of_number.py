b7ca78e19e37e693f424b5517b1b4bcc6596b914
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-12-17 00:39:44 +0600

    Creating sequence of array with the help of arange function

---
+++ Numpy/sequence_of_number.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#arange()
+# arange(start, stop, step)
+
+arr = np.arange(1, 11)  # Creating an array with numbers from 1 to 10 
+#it return a numpy array object
+print("Sequence of Numbers from 1 to 10:", arr)
+
+arr2 = np.arange(5, 21, 3)  # Creating an array from 5 to 20 with a step of 3
+print("Sequence of Numbers from 5 to 20 with step 3:", arr2)
